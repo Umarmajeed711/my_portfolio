075dd1c25173516baef8a485732570001d4eb3d3
Umarmajeed711 <umarmajeed711@gmail.com>
2026-01-05 09:58:28 +0500

    changes

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: window.location.href.split(":")[0] == "http" ? "http://localhost:5002/api/v1" : "/api/v1",
+    baseURL: window.location.href.split(":")[0] == "http" ? "http://localhost:5002/api/v1" : "https://my-portfolio-backend-nu-sand.vercel.app/api/v1",
     withCredentials: true,
 });
 
